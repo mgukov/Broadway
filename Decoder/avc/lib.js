30ef4a605371633accf38b856c75a7d369db9852
Michael Gukov <mgookov@gmail.com>
2020-02-13 01:19:46 +0500

    added yuv2rgb conversion

--- Decoder/avc/lib.js
+++ Decoder/avc/lib.js
@@ -2,8 +2,8 @@ var LibraryBroadway = {
   broadwayOnHeadersDecoded: function () {
     par_broadwayOnHeadersDecoded();
   },
-  broadwayOnPictureDecoded: function ($buffer, width, height) {
-    par_broadwayOnPictureDecoded($buffer, width, height);
+  broadwayOnPictureDecoded: function (id, $buffer, width, height) {
+    par_broadwayOnPictureDecoded(id, $buffer, width, height);
   },
   extern_emit_image: function($buffer, width, height, $otherBuffer){
     par_broadwayOnPictureDecoded($buffer, width, height, $otherBuffer);
--- Decoder/avc/src/Decoder.c
+++ Decoder/avc/src/Decoder.c
@@ -3,20 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <emscripten.h>
 
 /* Debug prints */
 #define DEBUG(argv) printf argv
 
 #include "opttarget.h"
-
-#include "extraFlags.h"
+#include "yuv_rgb.h"
 
 u32 broadwayInit();
-u32 broadwayDecode();
-void broadwayExit();
 
-const size_t STREAM_BUFFER_SIZE = 1024 * 1024;
-u8 *streamBuffer = NULL;
+u32 broadwayDecode();
+
+void broadwayExit();
 
 H264SwDecInst decInst;
 H264SwDecInput decInput;
@@ -35,69 +34,63 @@ typedef struct {
     u8 *end;
 } Stream;
 
+Stream broadwayStream;
+
+u32 rgb = 0;
+
 void streamInit(Stream *stream, u32 length) {
-    stream->buffer = stream->pos = (u8 *)malloc(sizeof(u8) * length);
+    stream->buffer = stream->pos = (u8 *) malloc(sizeof(u8) * length);
     stream->length = length;
     stream->end = stream->buffer + length;
 }
 
-void playStream(Stream *stream) {
+void playStream(u32 id, Stream *stream) {
     decInput.pStream = stream->buffer;
     decInput.dataLen = stream->length;
-    u32 i = 0;
     do {
-        u8 *start = decInput.pStream;
-        u32 ret = broadwayDecode();
-        // printf("Decoded Unit #%d, Size: %d, Result: %d\n", i++, (decInput.pStream - start), ret);
+        u32 ret = broadwayDecode(id);
     } while (decInput.dataLen > 0);
 }
 
+extern void broadwayOnHeadersDecoded();
 
-Stream broadwayStream;
+extern void broadwayOnPictureDecoded(u32 id, u8 *buffer, u32 width, u32 height);
+
 
 u8 *broadwayCreateStream(u32 length) {
     streamInit(&broadwayStream, length);
     return broadwayStream.buffer;
 }
 
-#ifdef LATENCY_OPTIMIZATION
-#endif
-
-
-void broadwayPlayStream(u32 length) {
+void broadwayPlayStream(u32 id, u32 length) {
     broadwayStream.length = length;
-    playStream(&broadwayStream);
+    playStream(id, &broadwayStream);
 }
 
-
-u32 broadwayInit() {
-  
-  H264SwDecRet ret;
+u32 broadwayInit(u32 _rgb) {
+    rgb = _rgb;
+    H264SwDecRet ret;
 #ifdef DISABLE_OUTPUT_REORDERING
-  u32 disableOutputReordering = 1;
+    u32 disableOutputReordering = 1;
 #else
-  u32 disableOutputReordering = 0;
+    u32 disableOutputReordering = 0;
 #endif
 
-  /* Initialize decoder instance. */
-  ret = H264SwDecInit(&decInst, disableOutputReordering);
-  if (ret != H264SWDEC_OK) {
-    DEBUG(("DECODER INITIALIZATION FAILED\n"));
-    broadwayExit();
-    return -1;
-  }
+    /* Initialize decoder instance. */
+    ret = H264SwDecInit(&decInst, disableOutputReordering);
+    if (ret != H264SWDEC_OK) {
+        emscripten_log(EM_LOG_ERROR, "DECODER INITIALIZATION FAILED");
+        broadwayExit();
+        return -1;
+    }
+    picDecodeNumber = picDisplayNumber = 1;
 
-  picDecodeNumber = picDisplayNumber = 1;
-
-  return 0;
+    emscripten_log(EM_LOG_CONSOLE, "init avc, rgb: %d", rgb);
+    return 0;
 }
 
 
-extern void broadwayOnHeadersDecoded();
-
-extern void broadwayOnPictureDecoded(u8 *buffer, u32 width, u32 height);
-
-u32 broadwayDecode() {
+u32 broadwayDecode(u32 id) {
     decInput.picId = picDecodeNumber;
 
     H264SwDecRet ret = H264SwDecDecode(decInst, &decInput, &decOutput);
@@ -130,12 +123,12 @@ u32 broadwayDecode() {
 
         case H264SWDEC_PIC_RDY:
             //if (ret == H264SWDEC_PIC_RDY) {
-                decInput.dataLen = 0;
+            decInput.dataLen = 0;
             //}
 
             /* Increment decoding number for every decoded picture */
             picDecodeNumber++;
-      
+
 
             while (H264SwDecNextPicture(decInst, &decPicture, 0) == H264SWDEC_PIC_RDY) {
                 // printf(" Decoded Picture Decode: %d, Display: %d, Type: %s\n", picDecodeNumber, picDisplayNumber, decPicture.isIdrPicture ? "IDR" : "NON-IDR");
@@ -144,31 +137,48 @@ u32 broadwayDecode() {
                 picDisplayNumber++;
 
 #ifndef EMIT_IMAGE_ASAP
-                broadwayOnPictureDecoded((u8*)decPicture.pOutputPicture, decInfo.picWidth, decInfo.picHeight);
+
+                if (rgb == 0) {
+                    broadwayOnPictureDecoded(id, (u8 *) decPicture.pOutputPicture, decInfo.picWidth, decInfo.picHeight);
+                } else {
+
+                    u32 stride = decInfo.picWidth;
+                    u32 stride2 = stride/2;
+
+                    u32 ylen = stride * decInfo.picHeight;
+                    u32 uvlen = stride2 * (decInfo.picHeight / 2);
+
+                    u8 *y = ((u8 *) decPicture.pOutputPicture);
+                    u8 *u = ((u8 *) decPicture.pOutputPicture) + ylen;
+                    u8 *v = ((u8 *) decPicture.pOutputPicture) + ylen + uvlen;
+
+
+                    u8 rgb[decInfo.picWidth * decInfo.picHeight * 4];
+                    yuv420_rgb32_std(decInfo.picWidth, decInfo.picHeight, y, u, v, stride, stride2, rgb,
+                                     stride * 4, YCBCR_601);
+                    broadwayOnPictureDecoded(id, rgb, decInfo.picWidth, decInfo.picHeight);
+                }
 #endif
             }
             break;
 
-      case H264SWDEC_STRM_PROCESSED:
-      case H264SWDEC_STRM_ERR:
-        /* Input stream was decoded but no picture is ready, thus get more data. */
-        decInput.dataLen = 0;
-        break;
-      
-      default:
-        break;
+        case H264SWDEC_STRM_PROCESSED:
+        case H264SWDEC_STRM_ERR:
+            /* Input stream was decoded but no picture is ready, thus get more data. */
+            decInput.dataLen = 0;
+            break;
+
+        default:
+            break;
     }
-  return ret;
+    return ret;
 }
 
 void broadwayExit() {
-    if (streamBuffer) {
-        free(streamBuffer);
-    }
 }
 
 u8 *broadwayCreateStreamBuffer(u32 size) {
-    u8 *buffer = (u8 *)malloc(sizeof(u8) * size);
+    u8 *buffer = (u8 *) malloc(sizeof(u8) * size);
     if (buffer == NULL) {
         DEBUG(("UNABLE TO ALLOCATE MEMORY\n"));
     }
